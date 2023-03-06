15977d6e7cefc7f6d2520313a31b055227af5927
CryptoFallen <richardtom1203@protonmail.com>
2023-03-06 15:01:01 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Basic Todo App
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### Links
+
+- [View Github Repository](https://github.com/r-tom90/basic-todo.git)
+- [View Live Site](https://richard-tom-portolio.vercel.app/)
+
+## My process
+
+### Built with
+
+- [React](https://reactjs.org/) - JS library
+- [Vite](https://vitejs.dev/) - React Bootstrap
+
+### What I learned
+
+React basics such as components, state, props. React Hooks lifecycle 'useEffect()' and using controlled HTML form in React.
+
+### Continued development
+
+- Implementation of:
+  - [x] Dark Mode
+  - Blogging.
+  - Web3 Authentication with NFT.
+
+## Author
+
+- Website - [Portfolio deployed with vercel](https://richard-tom-portolio.vercel.app/)
+- GitHub - [@r-tom90](https://github.com/r-tom90)
+- LinkedIn - [@richard-tom](https://www.linkedin.com/in/richard-tom-81b0956b/)
+- Twitter - [@CryptoFallen](https://twitter.com/CryptoFallen)
+- Medium - [@richardtom](https://medium.com/@richardtom_79153)
+
+## Acknowledgments
+
+This was built with the tutorial from the [DeployedByEd YouTube Channel](https://youtu.be/pCA4qpQDZD8).
---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  // States
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filteredHandler();
+    saveToLocal();
+  }, [todos, status]);
+
+  // Functions
+  const filteredHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  // Save Items to Local
+  const saveToLocal = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Rich's Todo List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
